0edc6ae913544f0d7ade20b30a3d20ba096b1fc3
Boris Wong <bobobwong@gmail.com>
2017-10-12 15:05:06 -0700

    Added inline elements example.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import SubApp from './SubApp.js'
 import SubApp2 from './SubApp2.js'
+import SubApp3 from './SubApp3'
+import * as Whatever from './SubApp3'
+
+Whatever.Hello2()
 
 class App extends Component {
+
   render() {
+    var line = (<span>This is text <br /></span>)
     return (
       <div className="App">
         <SubApp />
         <SubApp2 test2="this <b>is</b> here" />
-        <SubApp>Something else.</SubApp>
+        <SubApp>Something <SubApp3 /> else.</SubApp>
         <SubApp>Something other.</SubApp>
         <SubApp2>
           This is <u>SubApp2</u>
---
+++ src/SubApp3.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function SubApp3(props) {
+  return (
+    <span>
+      ThisIsSubApp3
+    </span>
+  )
+}
+
+export function Hello(laskdfj) {
+}
+
+export function Hello2() {
+}
+
+export default SubApp3
